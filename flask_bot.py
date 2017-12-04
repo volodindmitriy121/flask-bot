f6ed6a553e626c9209412447ad15dbdc63bb6340
volodindmitriy121 <volodindmitriy121@gmail.com>
2017-12-05 01:29:50 +0200

    init

---
+++ flask_bot.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+import telebot
+import os
+
+
+
+
+bot = telebot.TeleBot('368033691:AAFJux554qHs2WhDMde0OQfcQiklhN45xrY')
+
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://herokuProject_url/bot")
+    return "!", 200
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+server = Flask(__name__)
