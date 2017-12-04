fe7cc1dbbf7b33e88aebbb0f5f6a58d565799639
volodindmitriy121 <volodindmitriy121@gmail.com>
2017-12-05 01:34:00 +0200

    deploy

--- flask_bot.py
+++ flask_bot.py
@@ -25,7 +25,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://herokuProject_url/bot")
+    bot.set_webhook(url="https://eyezen-bot-flask.herokuapp.com/bot")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
