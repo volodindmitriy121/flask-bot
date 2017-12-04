97b553eeb42dab5935e2577f7920234526df9bab
volodindmitriy121 <volodindmitriy121@gmail.com>
2017-12-05 01:36:57 +0200

    test

--- flask_bot.py
+++ flask_bot.py
@@ -13,9 +13,13 @@ server = Flask(__name__)
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+#@bot.message_handler(func=lambda message: True, content_types=['text'])
+#def echo_message(message):
+#    bot.reply_to(message, message.text)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
